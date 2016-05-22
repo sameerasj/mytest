a2decc8b26c162986617fdfb9eb2b4f377cdc8d2
sameerasj <sameerajayawardana@gmail.com>
2016-05-22 18:25:33 +0530

    Initial commit

---
+++ src/test/java/testAppTest.java
@@ -0,0 +1,16 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+/**
+ * Created by chanakau on 5/22/2016.
+ */
+public class testAppTest
+{
+    @Test
+    public void firstTest()
+    {
+        WebDriver driver=new FirefoxDriver();
+        driver.get("http://google.com");
+    }
+}
